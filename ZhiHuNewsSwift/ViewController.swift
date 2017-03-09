dffdd314ee21463b1f657da6fbf8ddcc66d06fe7
ZJQian <2326274522@qq.com>
2017-03-09 09:20:45 +0800

    Initial Commit

---
+++ ZhiHuNewsSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZhiHuNewsSwift
+//
+//  Created by ZJQ on 2017/3/9.
+//  Copyright © 2017年 ZJQ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
