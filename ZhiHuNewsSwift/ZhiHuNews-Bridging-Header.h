ce547451e46d97e29f1a5b42f7b6f56287eeefd7
ZJQian <2326274522@qq.com>
2017-03-13 10:52:54 +0800

    桥文件

---
+++ ZhiHuNewsSwift/ZhiHuNews-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  ZhiHuNews-Bridging-Header.h
+//  ZhiHuNewsSwift
+//
+//  Created by ZJQ on 2017/3/10.
+//  Copyright © 2017年 ZJQ. All rights reserved.
+//
+
+#ifndef ZhiHuNews_Bridging_Header_h
+#define ZhiHuNews_Bridging_Header_h
+
+
+#import "SDCycleScrollView/SDCycleScrollView.h"
+
+
+#endif /* ZhiHuNews_Bridging_Header_h */
